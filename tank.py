ac48a940f71c78b8138cfb1631388c7f0f84ae90
Vinícius de Souza Godim de Oliveira <33293145+viniciusgodim@users.noreply.github.com>
2020-12-25 22:27:35 -0300

    Add files via upload

--- tank.py
+++ tank.py
@@ -2,7 +2,17 @@ import numpy as np
 from numpy.linalg import solve
 import matplotlib.pyplot as plt
 
-N = 25
+N = 20
+H = 1.75
+reynolds = 0.5e-2
+WD = 0.75
+nIterations = 300
+pRelax = 0.3
+uRelax = 0.3
+vRelax = 0.3
+uBC = [0, 0, 0, 0]
+vBC = [0, 0, 0, 0]
+
 
 vRange = np.array((3 + N) * [False] + (N * [True] +
                                        2 * [False]) * (N - 1) + (N + 1) * [False])
@@ -14,12 +24,8 @@ pRange = np.array((3 + N - 2) * [False] + ((N - 2) *
 Xp = np.linspace(1 / (2 * N), 1 - 1 / (2 * N), N)
 X = np.append(np.append(0, Xp), 1)
 
-H = 1.5
-
 Yp = np.linspace(H - H / (2 * N), H / (2 * N), N)
 Y = np.append(np.append(H, Yp), 0)
-
-WD = 0.25
 
 Rf = np.linspace(0, 1, N + 1)
 
@@ -37,25 +43,15 @@ AzP = Az[range(1, N), :]
 Vz = AzP * H / N
 Vz = Vz.flatten()
 
-nIterations = 300
-
-reynolds = 1
-
-pRelax = 0.5
-uRelax = 0.5
-vRelax = 0.5
-
 d = N / reynolds
 
 P = np.zeros((N, N))
 Uface = np.zeros((N, N - 1))
 Vface = np.zeros((N - 1, N))
 
-uBC = [0, 0, 0, 0]
 uBCArray = [np.ones((1, N + 2)) * uBC[0], np.ones((N, 1)) *
             uBC[1], np.ones((N, 1)) * uBC[2], np.ones((1, N + 2)) * uBC[3]]
 
-vBC = [0, 0, 0, 0]
 vBCArray = [np.ones((1, N)) * vBC[0], np.ones((N + 2, 1)) *
             vBC[1], np.ones((N + 2, 1)) * vBC[2], np.ones((1, N)) * vBC[3]]
 
@@ -63,7 +59,19 @@ vBCArray = [np.ones((1, N)) * vBC[0], np.ones((N + 2, 1)) *
 def movingAverage(x):
     return np.convolve(x, np.ones(2) / 2, mode='valid')
 
+
 Rfp = movingAverage(Rf)
+ArUAvg = np.apply_along_axis(movingAverage, 1, Ar).flatten()
+AzUAvg = np.apply_along_axis(movingAverage, 1, Az).flatten()
+ArVAvg = np.apply_along_axis(movingAverage, 0, Ar).flatten()
+AzVAvg = np.apply_along_axis(movingAverage, 0, Az).flatten()
+
+ArAux = Ar[range(1, N - 1), :][:, range(1, N)]
+AzAux = Az[range(1, N), :][:, range(1, N - 1)]
+
+Azflat = AzAux.flatten()
+Arflat = ArAux.flatten()
+
 
 def hybridScheme(f, d):
     return np.array([max(f[i], d[i] + f[i] / 2, 0) for i in range(4)])
@@ -80,11 +88,13 @@ def uSolve(uFace, vFace, p):
     vCornerLinear = vCorner.flatten()
     A = np.zeros((N * (N - 1), (N + 2) * (N + 1)))
     for i in range(N * (N - 1)):
-        D = np.ones(4) * d
         l = i // (N - 1)
-        radius = Rf[i % (l+1) + 1]
-        F = [-vCornerLinear[i], uNodeLinear[i + l], -
-             uNodeLinear[i + 1 + l], vCornerLinear[i + N - 1]]
+        area = np.array([AzUAvg[i], ArUAvg[i + l],
+                         ArUAvg[i + l + 1], AzUAvg[i + N - 1]])
+        D = area * d
+        radius = Rf[i % (N - 1) + 1]
+        F = np.array([-vCornerLinear[i], uNodeLinear[i + l], -
+                      uNodeLinear[i + 1 + l], vCornerLinear[i + N - 1]])
         wBC = np.ones(4)
         if i % (N - 1) == 0:
             wBC[1] = uBC[1]
@@ -102,8 +112,8 @@ def uSolve(uFace, vFace, p):
             aW = [0, 0, 0, -1]
             ap = 1
         if i + 1 > (N - 1) * (N - 1) and not wall:
-                aW = [-1, 0, 0, 0]
-                ap = 1
+            aW = [-1, 0, 0, 0]
+            ap = 1
         A[i, i + 1 + 2 * l] = aW[0]
         A[i, i + 1 + 2 * l + N] = aW[1]
         A[i, i + 2 + 2 * l + N] = ap
@@ -117,8 +127,7 @@ def uSolve(uFace, vFace, p):
     BU = BU + (np.diff(p, axis=1) * ArP).flatten() - \
         (1 - uRelax) * (ApU * uFace).flatten()
     for i in range(N * (N - 1)):
-        l = i // (N - 1)
-        radius = Rf[i % (l+1) + 1]
+        radius = Rf[i % (N - 1) + 1]
         wall = False
         if radius > WD:
             wall = True
@@ -138,11 +147,13 @@ def vSolve(uFace, vFace, p):
     vNodeLinear = vNode.flatten()
     A = np.zeros((N * (N - 1), (N + 2) * (N + 1)))
     for i in range(N * (N - 1)):
-        D = np.ones(4) * d
         l = i // N
-        radius = Rf[i % (l+1) + 1]
-        F = [-vNodeLinear[i], uCornerLinear[i + l], -
-             uCornerLinear[i + 1 + l], vNodeLinear[i + N]]
+        area = np.array([AzVAvg[i], ArVAvg[i + l],
+                         ArVAvg[i + l + 1], AzVAvg[i + N]])
+        D = area * d
+        radius = Rfp[i % N]
+        F = np.array([-vNodeLinear[i], uCornerLinear[i + l], -
+                      uCornerLinear[i + 1 + l], vNodeLinear[i + N]])
         wBC = np.ones(4)
         if (i + 1) % N == 0:
             D[2] *= 2
@@ -162,8 +173,8 @@ def vSolve(uFace, vFace, p):
             aW = [0, 0, -1, 0]
             ap = 1
         if (i + 1) > (N - 2) * N and not wall:
-                aW = [-1, 0, 0, 0]
-                ap = 1
+            aW = [-1, 0, 0, 0]
+            ap = 1
         A[i, i + 1 + 2 * l] = aW[0]
         A[i, i + 1 + 2 * l + N + 1] = aW[1]
         A[i, i + 2 + 2 * l + N + 1] = ap
@@ -177,8 +188,7 @@ def vSolve(uFace, vFace, p):
     BV = BV + Vz + (np.diff(p, axis=0) * AzP).flatten() - \
         (1 - vRelax) * (ApV * vFace).flatten()
     for i in range(N * (N - 1)):
-        l = i // N
-        radius = Rf[i % (l+1) + 1]
+        radius = Rfp[i % N]
         wall = False
         if radius > WD:
             wall = True
@@ -194,6 +204,8 @@ def vSolve(uFace, vFace, p):
 
 
 def pSolve(apU, apV, uFace, vFace):
+    apU = apU / ArP
+    apV = apV / AzP
     apU = apU[range(1, N - 1), :].flatten()
     apV = apV[:, range(1, N - 1)].flatten()
     uFaceStripped = uFace[range(1, N - 1), :]
@@ -203,8 +215,11 @@ def pSolve(apU, apV, uFace, vFace):
     A = np.zeros(((N - 2)**2, N**2))
     for i in range((N - 2)**2):
         l = i // (N - 2)
-        radius = Rfp[i % (l+1) + 1]
-        a = [r[i], q[i + l], q[i + 1 + l], r[i + N - 2]]
+        area = np.array([Azflat[i], Arflat[i + l],
+                         Arflat[i + 1 + l], Azflat[i + N - 2]])
+        radius = Rfp[i % (N - 2) + 1]
+        a = np.array([r[i], q[i + l], q[i + 1 + l], r[i + N - 2]])
+        a = a * area
         ap = -sum(a)
         if i + 1 <= N - 2:
             a[0] = 0
@@ -215,10 +230,8 @@ def pSolve(apU, apV, uFace, vFace):
             ap = 1
             a = [0, -1, 0, 0]
         if (i + 1) > (N - 2) * (N - 3):
-            wall = False
-            if radius > WD:
+            if radius <= WD:
                 a[3] = 0
-                wall = True
             else:
                 ap = 1
                 a = [-1, 0, 0, 0]
@@ -230,13 +243,19 @@ def pSolve(apU, apV, uFace, vFace):
     AP = A[:, pRange]
     BP = A[:, ~pRange]
     BP = -np.sum(BP, axis=1)
-    dU = np.diff(uFaceStripped, axis=1).flatten()
-    dV = np.diff(vFaceStripped, axis=0).flatten()
+    dU = np.diff(uFaceStripped * ArAux, axis=1).flatten()
+    dV = np.diff(vFaceStripped * AzAux, axis=0).flatten()
     BP = BP + dU + dV
     for i in range((N - 2)**2):
+        radius = Rfp[i % (N - 2) + 1]
+        wall = False
+        if radius > WD:
+            wall = True
         if i % (N - 2) == 0:
             BP[i] = 0
         if (i + 1) % (N - 2) == 0:
+            BP[i] = 0
+        if (i + 1) > (N - 2) * (N - 3) and wall:
             BP[i] = 0
     P = solve(AP, BP)
     P = np.reshape(P, (-1, N - 2))
@@ -245,7 +264,7 @@ def pSolve(apU, apV, uFace, vFace):
     for i in range(P.shape[1]):
         radius = Rfp[i]
         if radius > WD:
-            P[-1,i] = P[-2,i]
+            P[-1, i] = P[-2, i]
     P[0, 0] = (P[0, 1] + P[1, 0]) / 2
     P[-1, 0] = (P[-2, 0] + P[-1, 1]) / 2
     P[0, -1] = (P[1, -1] + P[0, -2]) / 2
@@ -257,6 +276,10 @@ for i in range(nIterations):
     print(i)
     uFaceHorizontalBounded = np.hstack((uBCArray[1], Uface, uBCArray[2])) * Ar
     vFaceVerticalBounded = np.vstack(([Vface[0, :]], Vface, vBCArray[3])) * Az
+    for i in range(vFaceVerticalBounded.shape[1]):
+        radius = Rfp[i]
+        if radius <= WD:
+            vFaceVerticalBounded[-1, i] = vFaceVerticalBounded[-2, i]
     USolution = uSolve(Uface, Vface, P)
     UfaceNew = USolution[0]
     UAp = USolution[1]
@@ -265,24 +288,35 @@ for i in range(nIterations):
     VAp = VSolution[1]
     pline = pSolve(UAp, VAp, UfaceNew, VfaceNew)
     P = P + pRelax * pline
-    Uface = UfaceNew - np.diff(pline, axis=1) / UAp
-    Vface = VfaceNew - np.diff(pline, axis=0) / VAp
+    Uface = UfaceNew - ArP * np.diff(pline, axis=1) / UAp
+    Vface = VfaceNew - AzP * np.diff(pline, axis=0) / VAp
     print(Vface)
 
 Uplot = np.hstack((uBCArray[1], Uface, uBCArray[2]))
 Uplot = np.apply_along_axis(movingAverage, 1, Uplot)
 Uplot = np.hstack((uBCArray[1], Uplot, uBCArray[2]))
-Uplot = np.vstack(([Uplot[0,:]], Uplot, [Uplot[-1,:]]))
-Vplot = np.vstack(([Vface[0,:]], Vface, vBCArray[3]))
-Vplot = np.apply_along_axis(movingAverage, 0, Vplot)
-Vplot = np.vstack(([Vplot[0,:]], Vplot, [Vplot[-1,:]]))
-Vplot = np.hstack(([[i] for i in Vplot[:,0]], Vplot, vBCArray[2]))
+Uplot = np.vstack(([Uplot[0, :]], Uplot, [Uplot[-1, :]]))
 
-Uplot = np.hstack((np.fliplr(Uplot)[:,:-1],Uplot))
-Vplot = np.hstack((np.fliplr(Vplot)[:,:-1],Vplot))
+for i in range(Uplot.shape[1] - 1):
+    radius = Rfp[i - 1]
+    if radius > WD:
+        Uplot[-1, i - 1] = uBC[3]
+
+Vplot = np.vstack(([Vface[0, :]], Vface, vBCArray[3]))
+Vplot = np.apply_along_axis(movingAverage, 0, Vplot)
+Vplot = np.vstack(([Vplot[0, :]], Vplot, [Vplot[-1, :]]))
+Vplot = np.hstack(([[i] for i in Vplot[:, 0]], Vplot, vBCArray[2]))
+
+for i in range(Vplot.shape[1] - 1):
+    radius = Rfp[i - 1]
+    if radius > WD:
+        Vplot[-1, i - 1] = vBC[3]
+
+Uplot = np.hstack((np.fliplr(Uplot)[:, :-1], -Uplot))
+Vplot = np.hstack((np.fliplr(Vplot)[:, :-1], Vplot))
 
 Xflip = -np.flip(X)[:-1]
-X = np.append(Xflip,X)
+X = np.append(Xflip, X)
 
 fig, ax = plt.subplots()
 ax.quiver(X, Y, Uplot, Vplot)
